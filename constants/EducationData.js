a783bf18ff2624073d26099cea3325a3eab170ab
IT21173936 <it21173936@my.sliit.lk>
2023-12-08 21:31:55 +0530

    my project

---
+++ constants/EducationData.js
@@ -0,0 +1,26 @@
+export const EducationData = [
+    {
+        name: "Bachelor of Science",
+        image:
+            "https://res.cloudinary.com/dbseotkid/image/upload/v1702040325/p8wr8csdzw8di1gmwjoe.jpg",
+        schoolOrCollege: "SRI LANKA INSTITUTE OF INFORMATION TECHNOLOGY (SLIIT)",
+        fromTo: "2021 - 2025",
+        statusOrPrecentage: "Dropped in second year",
+    },
+    {
+        name: "Senior Secondary Examination",
+        image:
+            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219649/jkjfvdlxjntnyrxs57ia.png",
+        schoolOrCollege: "SWAMI TEONRAM ALOK SR. SEC. SCHOOL DADABARI, KOTA",
+        fromTo: "2019",
+        statusOrPrecentage: "96.80 %",
+    },
+    {
+        name: "Secondary Examination",
+        image:
+            "http://res.cloudinary.com/dhixp5pnk/image/upload/v1696219649/tnverjapfr4slg7fnfcw.png",
+        schoolOrCollege: "A.V.M. SECONDARY SCHOOL, RAWATBHATA",
+        fromTo: "2016",
+        statusOrPrecentage: "93.33%",
+    },
+];
